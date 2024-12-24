c7d3cfa627dfa1cb453886585fbde8158eb21b09
gupi debnath <iamgupidebnath@gmail.com>
2024-12-24 13:19:52 +0530

    add custom-theme

--- src/atoms/RadioButton/RedioButtonStyle.tsx
+++ src/atoms/RadioButton/RedioButtonStyle.tsx
@@ -1,17 +1,20 @@
 import { styled } from '@mui/material/styles';
 import { FormControl, FormLabel, RadioGroup } from '@mui/material';
+import { theme as customTheme } from '../../thems/primitives/theme';
 
-export const StyledFormControl = styled(FormControl)({
-  marginBottom: '16px',
-});
+export const StyledFormControl = styled(FormControl)(({ theme }) => ({
+  marginBottom: customTheme.spacing(2),
+}));
 
-export const StyledFormLabel = styled(FormLabel)({
-  fontSize: '16px',
-  fontWeight: 'bold',
-  fontFamily: 'sans-serif',
-  marginBottom: '8px',
-});
+export const StyledFormLabel = styled(FormLabel)(({ theme }) => ({
+  fontSize: customTheme.typography.body1.fontSize,
+  fontWeight: customTheme.typography.fontWeightBold,
+  fontFamily: customTheme.typography.fontFamily,
+  marginBottom: customTheme.spacing(1),
+}));
 
-export const StyledRadioGroup = styled(RadioGroup)({
+export const StyledRadioGroup = styled(RadioGroup)(({ theme }) => ({
   display: 'flex',
-});
+  flexDirection: 'row',
+  gap: customTheme.spacing(1),
+}));
--- src/atoms/TextInput/TextInput.tsx
+++ src/atoms/TextInput/TextInput.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
+import { useTheme } from '@mui/material';
 
 import {
   TextFieldWrapper,
   InputLabelStyled,
   StyledInput,
+  ErrorMessage,
 } from './TextInput.style';
+import useEmailValidation from './customHooks';
 
 type TextInputProps = {
   label?: string;
   placeholder?: string;
   backgroundColor?: string;
+  labelColor?: string;
+  labelFontSize?: string;
+  inputWidth?: string;
+  inputPadding?: string;
+  inputBorderColor?: string;
+  inputBorderRadius?: string;
+  inputFontSize?: string;
+
+  type?: 'text' | 'email' | 'password' | 'number';
 };
 
 const TextInput = (props: TextInputProps) => {
+  const { error, validateEmail } = useEmailValidation();
+
+  const handleChange = (event: any) => {
+    const value = event.target.value;
+    if (props.type === 'email') {
+      validateEmail(value);
+    }
+  };
+  const theme: string | number | any = useTheme();
+  console.log(theme);
+
   return (
     <TextFieldWrapper backgroundColor={props.backgroundColor}>
-      <InputLabelStyled htmlFor={props.label}>{props.label}</InputLabelStyled>
+      <InputLabelStyled
+        style={{}}
+        htmlFor={props.label}
+        labelColor={props.labelColor}
+        fontSize={props.labelFontSize}
+      >
+        {props.label}
+      </InputLabelStyled>
       <StyledInput
         id={props.label}
-        type="text"
+        type={props.type || 'text'}
         placeholder={props.placeholder || props.label}
+        width={props.inputWidth}
+        padding={props.inputPadding}
+        borderColor={error ? '#FF0000' : props.inputBorderColor}
+        borderRadius={props.inputBorderRadius}
+        fontSize={props.inputFontSize}
+        onChange={handleChange}
       />
+      {error && <ErrorMessage>{error}</ErrorMessage>}
     </TextFieldWrapper>
   );
 };
