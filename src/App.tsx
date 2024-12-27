9c4a7661e8382557c7e62b379dcd69812f6d8977
gupi debnath <iamgupidebnath@gmail.com>
2024-12-27 17:57:11 +0530

    login-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { useTheme } from "@mui/material";
+import React from 'react';
+import { useTheme } from '@mui/material';
+import LoginPage from './atoms/Login/Login';
 const App = () => {
   const theme: any = useTheme();
-  console.log(theme, "theme");
+  console.log(theme, 'theme');
   return (
     <div>
-      <h1 style={{ color: theme.palette.main }}>
+      {/* <h1 style={{ color: theme.palette.main }}>
         Hello, React with TypeScript!
-      </h1>
+      </h1> */}
+      <LoginPage />
     </div>
   );
 };
